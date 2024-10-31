c9e1d8ed3f309e661fd62dd5d7c3bbf954b924a8
StefanSaladino <200551988@student.georgianc.on.ca>
2024-10-31 19:24:40 -0400

    altered middleware configuration

--- project-spa-backend/routes/components.js
+++ project-spa-backend/routes/components.js
@@ -7,7 +7,7 @@ const User = require("../models/user");
 const dotenv = require("dotenv");
 
 // Middleware to ensure the user is authenticated
-// router.use(ensureAuthenticated);
+router.use(ensureAuthenticated);
 
 // Define routes for different parts of the resume
 // Apply the token verification middleware to all /resume routes
